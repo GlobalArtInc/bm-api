7558bc3940379348fdbb1c0050f43d69f878d6a8
Roman <roman@ga-games.com>
2022-04-10 09:44:36 -0700

    fix

--- examples/get-profile.ts
+++ examples/get-profile.ts
@@ -1,7 +1,7 @@
-import { Client } from '../src'
+import {Client} from '../src'
 
 const client = new Client({
-    token: 'RypWZQHdfhVttOAWs77EjJ69Er4W3lMW1G3xZey6EBA2q2cTFdNGiVVv7aDJwa4Zfj7A767DVGVuebFPaWu1faPY5A6hIWPPkdlCYw5JIPPeJVK4fzdkMHzm5dfBe2Q5'
+    token: 'BOT_TOKEN_IS_HERE'
 })
 
 client.users.profile("189094417712414720").then((res) => {
